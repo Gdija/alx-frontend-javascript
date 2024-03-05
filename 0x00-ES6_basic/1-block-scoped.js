71b6905e1ab7da506283389bb992081f9113959c
Gdija <khadijaghadi00@gmail.com>
2024-03-05 01:36:07 +0100

    update task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,11 @@
+/* eslint no-unused-vars: "error" */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
     const task = true;
+    // eslint-disable-next-line no-unused-vars
     const task2 = false;
   }
   return [task, task2];
