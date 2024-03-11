5f6469e72e8dfdf4323379c9d01b21ba2d6a3df5
Gdija <khadijaghadi00@gmail.com>
2024-03-11 13:33:30 +0000

    task3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,5 @@
+export default function getStudentIdsSum(arr) {
+  const initialValue = 0;
+  const Newarr = arr.reduce((accumulator, currentValue) => accumulator + currentValue.id, initialValue  ,);
+  return Newarr;
+}
