40666c08e421833b3a83ef844929b0892fac7698
Gdija <khadijaghadi00@gmail.com>
2024-03-12 10:54:20 +0000

    update file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
+  if (typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0) {
     return '';
   }
   const foundValues = Array.from(set)
