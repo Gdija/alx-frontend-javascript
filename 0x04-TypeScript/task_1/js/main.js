9bab4af3f9936124856086a1ac77bab7705d9488
Gdija <khadijaghadi00@gmail.com>
2024-03-15 10:52:42 +0000

    task4

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,4 +1,23 @@
+"use strict";
+exports.__esModule = true;
+exports.StudentClass = exports.printTeacher = void 0;
 function printTeacher(firstName, lastName) {
     return "".concat(firstName[0], ". ").concat(lastName);
 }
+exports.printTeacher = printTeacher;
 ;
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    ;
+    StudentClass.prototype.displayName = function () {
+        return "".concat(this.firstName);
+    };
+    return StudentClass;
+}());
+exports.StudentClass = StudentClass;
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,9 +7,33 @@ interface Teacher {
     [attName: string]: any;
 }
 
-interface printTeacherFunction{
+export interface printTeacherFunction{
     (firstName: string, lastName: string): string
 }
-function printTeacher(firstName: string, lastName:string): string{
+export function printTeacher(firstName: string, lastName:string): string{
     return`${firstName[0]}. ${lastName}`;
 };
+export interface StudentClassConstructor{
+    new (firstName: string, lastName: string): StudentClass;
+}
+export interface StudentClassInter{
+    workOnHomework(): string;
+    displayName(): string;
+}
+export class StudentClass implements StudentClassInter{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string{
+        return 'Currently working';
+    };
+    displayName(){
+        return `${this.firstName}`;
+    }
+
+
+}
